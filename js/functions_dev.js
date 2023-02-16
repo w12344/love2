0af7a76722dcce265b705d675a1ea76981bdf0d4
w12344 <40829345+w12344@users.noreply.github.com>
2023-02-16 13:45:49 +0800

    Update functions_dev.js

--- js/functions_dev.js
+++ js/functions_dev.js
@@ -95,7 +95,8 @@ function startHeartAnimation() {
 function timeElapse(date){
 	var current = Date();
 	var seconds = (Date.parse(current) - Date.parse(date)) / 1000;
-	var days = Math.floor(seconds / (3600 * 24));
+	var years = Math.floor(seconds / (3600 * 24 * 365));
+	var days = Math.floor((seconds % (3600 * 24 * 365)) / (3600 * 24));
 	seconds = seconds % (3600 * 24);
 	var hours = Math.floor(seconds / 3600);
 	if (hours < 10) {
@@ -110,7 +111,7 @@ function timeElapse(date){
 	if (seconds < 10) {
 		seconds = "0" + seconds;
 	}
-	var result = "<span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
+	var result = "<span class=\"digit\">" + years + "</span> years <span class=\"digit\">" + days + "</span> days <span class=\"digit\">" + hours + "</span> hours <span class=\"digit\">" + minutes + "</span> minutes <span class=\"digit\">" + seconds + "</span> seconds"; 
 	$("#elapseClock").html(result);
 }
 
